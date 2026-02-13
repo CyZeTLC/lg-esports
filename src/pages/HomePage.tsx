7277ad323cbf331e03753dda58c9f6e8f69372c9
CyZeTLC <tom@cyzetlc.de>
2026-02-13 08:22:49 +0100

    added background image to homeview

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,6 +6,8 @@ function HomePage() {
         <div className="min-h-screen bg-gh-bg flex flex-col">
             <Header />
 
+            <img src="https://preview.redd.it/dx7rtr7pfwq41.png?width=1080&crop=smart&auto=webp&s=9e44dbc8563374344b313e4b6574c7e33ba346d6" alt="Lonely Gods Esports Banner" className="w-full h-64 object-cover" />
+
             <main className="bg-gh-bg text-gh-text p-8">
                 <h1 className="text-4xl font-bold mb-4">Lonely Gods Esports</h1>
             </main>
